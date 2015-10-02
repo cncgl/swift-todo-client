aee42aed628fdee928bee115cd9acd15515b8659
cncgl <concigel@gmail.com>
2015-10-02 23:38:55 +0900

    init

--- todo-client2/ViewController.swift
+++ todo-client2/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadJson()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func loadJson() {
+        let url = "https://192.168.24.42:9443/api/todos"
+        //let queue   = NSOperationQueue.mainQueue()
+        let config  = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        let request = NSURLRequest(URL: NSURL(string: url)!)
+        //NSURLConnection.sendAsynchronousRequest(request, queue:queue)
+        let task = session.dataTaskWithRequest(request, completionHandler: {
+            (response, data, error) in
+                //let jsonString = NSString(data: data!, encoding: NSASCIIStringEncoding) as! String
+                
+                let json = JSON(url: url)
+                
+                print(json)
+            
+            })
+        task.resume()
+    }
 
 }
 
